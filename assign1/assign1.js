ac161b4b60ee4d7f9589552b9c6ef00395e4c093
chandan <chandantech56@gmail.com>
2025-01-29 20:42:27 +0530

    js

---
+++ assign1/assign1.js
@@ -0,0 +1,13 @@
+console.log("Welcome to xyz company");
+document.write("Welcome to javascript course");
+let EmployeeName= "Chandan";
+let id= 123;
+let employeeAdress="Bangalore";
+let IsManager= true;
+let mobileNumber= 8088888888;
+console.log("Type of EmpName:",typeof(EmployeeName));
+console.log("Type of id:",typeof(id));
+console.log("Type of EmpAdd:",typeof(employeeAdress));
+console.log("Type of IsManager:",typeof(IsManager));
+console.log("Type of mobileNumber:",typeof(mobileNumber));
+
---
+++ index.js
@@ -0,0 +1,19 @@
+// print in console
+console.log("hello i am a javascript");
+
+// print in web page
+document.write("hello!! i am a javascript external file");
+// example datatypes
+let FirstName= "Chandan";
+let age= 30;
+let isAdmin= false;
+let project= null;
+let salary;
+let price= 87997.21
+// Find the data Types.....
+console.log("FirstNmae dataTypes is:", typeof(FirstName)); // Output is string
+console.log("age dataTypes is:", typeof(age));             // Output is Number
+console.log("isAdmin dataTypes is:", typeof(isAdmin));     // Output is Boolien
+console.log("project dataTypes is:", typeof(project));     // Output is null
+console.log("salary dataTypes is:", typeof(salary));       // Output is undefined
+console.log("price dataTypes is:", typeof(price));         // Output is Number
