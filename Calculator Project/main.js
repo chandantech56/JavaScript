e12a2764745905a09598ff314b39a5752dba3e22
chandantech56 <chandantech56@gmail.com>
2025-03-26 16:54:58 +0530

    todo list

--- Calculator Project/main.js
+++ Calculator Project/main.js
@@ -3,7 +3,7 @@ let string = " ";
 let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button) =>{
     button.addEventListener('click', (e)=>{
-        if(e.target.innerHTML == '='){
+        if(e.target.innerHTML === '='){
             string = eval(string);
             document.querySelector('input').value = string;
         }
@@ -12,10 +12,11 @@ Array.from(buttons).forEach((button) =>{
             string = " ";
             document.querySelector('input').value = string;
         }
-        else if(e.target.innerHTML == '%'){
+        else if(e.target.innerHTML === '%'){
             string = eval(string);
             document.querySelector('input').value = string;
         }
+        
         else{
         console.log(e.target)
         string = string + e.target.innerHTML;
